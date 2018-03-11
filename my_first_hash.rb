e4cce2aa82a4db6eb8c9eb6221b915150481216e
Yolanda Galdamez <yolanda.v.galdamez@gmail.com>
2018-03-11 04:34:03 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  myself = {"name" => "Yolanda", "age" => "32", "hair_color" => "brown"}
   
 end
 
@@ -9,8 +9,11 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  the_manifest =  {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }
 end
 
 def retrieval
@@ -19,7 +22,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
+  shipping_manifest["oil_paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
